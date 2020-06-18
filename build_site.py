0ee9c9431df1cf934199e7bc1fd4e46524448c73
kylepollina <>
2020-06-18 18:37:27 -0500

    create page class

--- build_site.py
+++ build_site.py
@@ -3,22 +3,55 @@ Uses jinja2 to build a static website
 """
 
 import os
-from shutil import copyfile
+import shutil
 from jinja2 import Environment, FileSystemLoader
-
-env = Environment(
-    loader=FileSystemLoader('templates'),
-    trim_blocks=True,
-    lstrip_blocks=True,
-)
 
 os.chdir('..')
 
+class Page:
+    env = Environment(
+        loader=FileSystemLoader('templates'),
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+
+    def __init__(self, name, directory, page, requires_p5, styles=True):
+        self.name = name
+        self.directory = directory
+        self.page = page
+        self.requires_p5 = requires_p5
+        self.styles = styles
+
+    def build(self):
+        if self.styles:
+            page.copy_styles()
+        file_contents = self.build_content()
+        self.write_file(file_contents)
+
+    def copy_styles(self):
+        """Copy the styles.css file into the directory"""
+        shutil.copyfile('styles.css', f'../{self.directory}/styles.css')
+
+    def build_content(self) -> str:
+        """Generate the file contents"""
+        template = self.env.get_template(self.name)
+        file_contents = template.render(
+            file_name=self.name,
+            current_page=self.page,
+            title=self.name,
+            requires_p5=self.requires_p5,
+            sketch_name=self.sketch_name,
+            utility_scripts=[
+                'palettes.js'
+            ]
+        )
+
+        return file_contents
+
 def main():
     """Build each individual site"""
-    copy_styles()
     kyle_pollina_github_io()
-    art()
+    # art()
     # build_kinect()
     # build_color_palettes()
     # build_mandalas()
@@ -30,70 +63,49 @@ def write_file(directory, file_name, file_contents):
     with open(file_name, "w+") as f:
         f.write(file_contents)
 
-def copy_styles(directory):
-    copyfile('kyle_pollina_github_io/styles.css', f'{directory}/styles.css')
-
 def kyle_pollina_github_io():
-    directory = 'kylepollina.github.io'
-    file_name = 'index.html'
+    page = Page(name='index.html', directory='kylepollina.github.io', page='home', requires_p5=True)
+    page.build()
 
-    template = env.get_template(file_name)
-    copy_styles(directory)
+# def art():
+#     directory = 'kylepollina.github.io'
+#     file_name = 'index.html'
 
-    file_contents = template.render(
-        file_name=file_name,
-        current_page='home',
-        title='',
-        requires_p5=True,
-        is_sketch=False,
-        sketch_name='main-sketch',
-        utility_scripts=[
-            'palettes.js'
-        ]
-    )
+#     template = env.get_template(file_name)
+#     copy_styles(directory)
 
-    write_file(directory, file_name, file_contents)
+#     sketches = [
+#         {'sketch_name': 'thrill',    'scripts': []},
+#         {'sketch_name': 'squares',   'scripts': ['palettes.js','io.js']},
+#         {'sketch_name': 'corners',   'scripts': ['palettes.js','grid.js']},
+#         {'sketch_name': 'arcs',      'scripts': []},
+#         {'sketch_name': 'towers',    'scripts': ['grid.js']},
+#         {'sketch_name': 'gogh',      'scripts': ['phyllotaxis.js','graphics.js','d3-delaunay.js']},
+#         {'sketch_name': 'diamond',   'scripts': ['palettes.js']},
+#         {'sketch_name': 'starry',    'scripts': ['phyllotaxis.js','graphics.js','shapes.js']},
+#         {'sketch_name': 'triangles', 'scripts': []},
+#         {'sketch_name': 'holohex',   'scripts': ['palettes.js','turtle.js','shapes.js']},
+#         {'sketch_name': 'spiro',     'scripts': ['turtle.js','shapes.js']},
+#         {'sketch_name': 'lisa',      'scripts': ['graphics.js','shapes.js']},
+#         {'sketch_name': 'breathing', 'scripts': []},
+#     ]
 
-def art():
-    directory = 'kylepollina.github.io'
-    file_name = 'index.html'
-
-    template = env.get_template(file_name)
-    copy_styles(directory)
-
-    sketches = [
-        {'sketch_name': 'thrill',    'scripts': []},
-        {'sketch_name': 'squares',   'scripts': ['palettes.js','io.js']},
-        {'sketch_name': 'corners',   'scripts': ['palettes.js','grid.js']},
-        {'sketch_name': 'arcs',      'scripts': []},
-        {'sketch_name': 'towers',    'scripts': ['grid.js']},
-        {'sketch_name': 'gogh',      'scripts': ['phyllotaxis.js','graphics.js','d3-delaunay.js']},
-        {'sketch_name': 'diamond',   'scripts': ['palettes.js']},
-        {'sketch_name': 'starry',    'scripts': ['phyllotaxis.js','graphics.js','shapes.js']},
-        {'sketch_name': 'triangles', 'scripts': []},
-        {'sketch_name': 'holohex',   'scripts': ['palettes.js','turtle.js','shapes.js']},
-        {'sketch_name': 'spiro',     'scripts': ['turtle.js','shapes.js']},
-        {'sketch_name': 'lisa',      'scripts': ['graphics.js','shapes.js']},
-        {'sketch_name': 'breathing', 'scripts': []},
-    ]
-
-    build_interactive_main(sketches)
-    build_interactive_sketches(sketches)
+#     build_interactive_main(sketches)
+#     build_interactive_sketches(sketches)
 
 
-def build_interactive_main(sketches):
-    sketch_names = [sketch['sketch_name'] for sketch in sketches]
+# def build_interactive_main(sketches):
+#     sketch_names = [sketch['sketch_name'] for sketch in sketches]
 
-    file_name = 'in.html'
-    template = env.get_template(file_name)
-    file_contents = template.render(
-        file_name=file_name,
-        current_page='interactive',
-        title='kyle pollina',
-        p5js=False,
-        is_sketch=False,
-        posts=sketch_names
-    )
+#     file_name = 'in.html'
+#     template = env.get_template(file_name)
+#     file_contents = template.render(
+#         file_name=file_name,
+#         current_page='interactive',
+#         title='kyle pollina',
+#         p5js=False,
+#         posts=sketch_names
+#     )
 
     # write_file(BASE_PATH / file_name, file_contents)
 
