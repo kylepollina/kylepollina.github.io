8ae5edf05889700683f7e61d37558ef5c019ea6b
kylepollina <>
2020-06-26 18:43:27 -0500

    ukiyo-e update
    cleanup

--- build_site.py
+++ build_site.py
@@ -5,9 +5,6 @@ Uses jinja2 to build a static website
 import shutil
 from jinja2 import Environment, FileSystemLoader
 
-# from rich.traceback import install
-# install()
-
 class Page:
     env = Environment(
         loader=FileSystemLoader('templates'),
@@ -15,7 +12,7 @@ class Page:
         lstrip_blocks=True,
     )
 
-    def __init__(self, directory, file_name, page, template_name, styles=True, requires_p5=False, sketch_name=None, is_sketch=False, utility_scripts=None, posts=None):
+    def __init__(self, directory, file_name, page, template_name, styles=True, requires_p5=False, sketch_name=None, is_sketch=False, utility_scripts=None, posts=None, hiroshige=None, narumi=None):
         self.directory = directory
         self.file_name = file_name
         self.page = page
@@ -24,6 +21,8 @@ class Page:
         self.requires_p5 = requires_p5
         self.sketch_name = sketch_name
         self.is_sketch = is_sketch
+        self.hiroshige = hiroshige
+        self.narumi = narumi
         self.utility_scripts = utility_scripts if utility_scripts is not None else []
         self.posts = posts
 
@@ -51,7 +50,9 @@ class Page:
             requires_p5=self.requires_p5,
             sketch_name=self.sketch_name,
             utility_scripts=self.utility_scripts,
-            posts=self.posts
+            posts=self.posts,
+            hiroshige=self.hiroshige,
+            narumi=self.narumi,
         )
 
         return file_contents
@@ -65,24 +66,52 @@ class Page:
 
 def kylepollina_github_io():
     Page(
-        directory='kylepollina.github.io', 
-        file_name='index.html', 
-        page='home', 
-        template_name='kylepollina.github.io.html', 
-        requires_p5=True, 
+        directory='kylepollina.github.io',
+        file_name='index.html',
+        page='home',
+        template_name='kylepollina.github.io.html',
+        requires_p5=True,
         is_sketch=True,
-        sketch_name='main-sketch', 
+        sketch_name='main-sketch',
         utility_scripts=['palettes.js']
     ).build()
 
+
 def earthscience():
     Page(
-        directory='earthscience', 
-        file_name='index.html', 
-        page='earthscience', 
-        template_name='earthscience.html', 
+        directory='earthscience',
+        file_name='index.html',
+        page='earthscience',
+        template_name='earthscience.html',
     ).build()
 
+
+def ukiyo_e():
+    Page(
+        directory='ukiyo-e',
+        file_name='index.html',
+        page='ukiyo-e',
+        template_name='ukiyo-e.html',
+        hiroshige=[
+            'No. 2 Kawachi Settsu',
+            'Tagged Cranes',
+        ],
+        narumi=[
+            'Postcard of Genroku Beauty 2',
+            'Woman with umbrella',
+            'Postcard of Princess from Jogaku sekai',
+            'Female nude seated in water',
+        ]
+    ).build()
+
+if __name__ == "__main__":
+    """Build each individual site"""
+    print('building kylepollina.github.io')
+    kylepollina_github_io()
+    print('building earthscience')
+    earthscience()
+    print('building ukiyo-e')
+    ukiyo_e()
 
 
 # def build_color_palettes():
@@ -119,26 +148,6 @@ def earthscience():
     # write_file(BASE_PATH / file_name, file_contents)
 
 
-# def build_ukiyo_e():
-    # file_name = 'ukiyo-e.html'
-    # template = env.get_template(file_name)
-    # file_contents = template.render(
-    #     file_name=file_name,
-    #     current_page='ukiyo-e',
-    #     title='kyle pollina',
-    #     p5js=False,
-    #     hiroshige=[
-    #         'No. 2 Kawachi Settsu',
-    #         'Tagged Cranes',
-    #     ],
-    #     narumi=[
-    #         'Postcard of Genroku Beauty 2',
-    #         'Woman with umbrella',
-    #         'Postcard of Princess from Jogaku sekai',
-    #         'Female nude seated in water',
-    #     ]
-    # )
-
     # write_file(BASE_PATH / file_name, file_contents)
 
 
@@ -170,60 +179,42 @@ def earthscience():
 
     # write_file(BASE_PATH / ('data_science/' + file_name), file_contents)
 
-if __name__ == "__main__":
-    """Build each individual site"""
-    print('building kylepollina.github.io')
-    kylepollina_github_io()
-    print('building earthscience')
-    earthscience()
+# def art():
+#     sketches = [
+#         {'sketch_name': 'thrill',    'scripts': []},
+#         {'sketch_name': 'squares',   'scripts': ['palettes.js','io.js']},
+#         {'sketch_name': 'corners',   'scripts': ['palettes.js','grid.js']},
+#         {'sketch_name': 'arcs',      'scripts': []},
+#         {'sketch_name': 'towers',    'scripts': ['grid.js']},
+#         {'sketch_name': 'gogh',      'scripts': ['phyllotaxis.js','graphics.js','d3-delaunay.js']},
+#         {'sketch_name': 'diamond',   'scripts': ['palettes.js']},
+#         {'sketch_name': 'starry',    'scripts': ['phyllotaxis.js','graphics.js','shapes.js']},
+#         {'sketch_name': 'triangles', 'scripts': []},
+#         {'sketch_name': 'holohex',   'scripts': ['palettes.js','turtle.js','shapes.js']},
+#         {'sketch_name': 'spiro',     'scripts': ['turtle.js','shapes.js']},
+#         {'sketch_name': 'lisa',      'scripts': ['graphics.js','shapes.js']},
+#         {'sketch_name': 'breathing', 'scripts': []},
+#     ]
 
+#     sketch_names = [sketch['sketch_name'] for sketch in sketches]
 
+#     Page(
+#         directory='art',
+#         file_name='index.html',
+#         page='art',
+#         template_name='art.html',
+#         posts=sketch_names
+#     ).build()
 
-    # print('building art')
-    # art()
-    # build_kinect()
-    # build_color_palettes()
-    # build_mandalas()
-    # build_ukiyo_e()
-    # build_data_science()
-
-def art():
-    sketches = [
-        {'sketch_name': 'thrill',    'scripts': []},
-        {'sketch_name': 'squares',   'scripts': ['palettes.js','io.js']},
-        {'sketch_name': 'corners',   'scripts': ['palettes.js','grid.js']},
-        {'sketch_name': 'arcs',      'scripts': []},
-        {'sketch_name': 'towers',    'scripts': ['grid.js']},
-        {'sketch_name': 'gogh',      'scripts': ['phyllotaxis.js','graphics.js','d3-delaunay.js']},
-        {'sketch_name': 'diamond',   'scripts': ['palettes.js']},
-        {'sketch_name': 'starry',    'scripts': ['phyllotaxis.js','graphics.js','shapes.js']},
-        {'sketch_name': 'triangles', 'scripts': []},
-        {'sketch_name': 'holohex',   'scripts': ['palettes.js','turtle.js','shapes.js']},
-        {'sketch_name': 'spiro',     'scripts': ['turtle.js','shapes.js']},
-        {'sketch_name': 'lisa',      'scripts': ['graphics.js','shapes.js']},
-        {'sketch_name': 'breathing', 'scripts': []},
-    ]
-
-    sketch_names = [sketch['sketch_name'] for sketch in sketches]
-
-    Page(
-        directory='art', 
-        file_name='index.html', 
-        page='art', 
-        template_name='art.html', 
-        posts=sketch_names
-    ).build()
-
-    for sketch in sketches:
-        Page(
-            directory='art/interactive', 
-            file_name=f'{sketch["sketch_name"]}.html', 
-            page='art', 
-            template_name='sketch.html', 
-            styles=False,
-            requires_p5=True,
-            sketch_name=sketch['sketch_name'],
-            is_sketch=True,
-            utility_scripts=sketch['scripts']
-        ).build()
-
+#     for sketch in sketches:
+#         Page(
+#             directory='art/interactive',
+#             file_name=f'{sketch["sketch_name"]}.html',
+#             page='art',
+#             template_name='sketch.html',
+#             styles=False,
+#             requires_p5=True,
+#             sketch_name=sketch['sketch_name'],
+#             is_sketch=True,
+#             utility_scripts=sketch['scripts']
+#         ).build()
