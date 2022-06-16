1a7ba3ded9537285f4553d770c9b5d3b07765343
kyle pollina <>
2022-06-16 15:04:35 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
     content: ["./**/*.html", "./content/**/*.md"],
     theme: {
-      extend: {},
+      // colors: {
+      //   'darkred': '#CE2029'
+      // },
+      extend: {
+        colors: {
+          'darkred': '#CE2029',
+          'lightblue': 'rgb(102,172,228)'
+        }
+      },
     },
     plugins: [],
 }
